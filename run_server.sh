3a49c28cd879aa94132d310e6d2f7080316f1c9c
Lewis Daly <lewisdaly@me.com>
2016-05-10 16:59:24 +0930

    docker setup

---
+++ run_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$1" == "build" ]
+then
+  echo "building server"
+  docker-compose build
+  docker-compose pull
+fi
+
+if [ "$1" == "clear" ]
+then
+  echo "tearing down and rebuilding server!"
+  docker-compose rm -fv
+  docker-compose build
+  docker-compose pull
+fi
+
+docker-compose up
