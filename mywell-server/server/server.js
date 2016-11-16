53be3e453d3ab6e5c2e38415af7d49598d1a0125
Lewis Daly <lewis.daly@enabled.com.au>
2016-11-16 20:17:28 +1030

    added http logging

--- mywell-server/server/server.js
+++ mywell-server/server/server.js
@@ -8,7 +8,7 @@ app.start = function() {
   return app.listen(function() {
     app.emit('started');
 
-    
+
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
@@ -28,4 +28,10 @@ boot(app, __dirname, function(err) {
   // start the server if `$ node server.js`
   if (require.main === module)
     app.start();
+});
+
+app.middleware('initial', '/api/*', function logResponse(req, res, next) {
+  // same code as sample # 2
+  console.log('url is:', req.protocol + '://' + req.get('host') + req.originalUrl);
+  next();
 });
