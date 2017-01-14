f4916a06b6423b68761e0d72425b6b4e6db38a4d
Lewis Daly <lewis.daly@enabled.com.au>
2017-01-14 15:55:17 +1030

    update circle config

--- mywell-server/server/boot/db/wait_for_db.js
+++ mywell-server/server/boot/db/wait_for_db.js
@@ -1,17 +1,22 @@
+
 const _waitForDataBaseConnection = (db, ttl) => {
   return new Promise ((resolve, reject) => {
+    console.log("pinging db");
     db.ping((err) => {
+      console.log("pinged db");
       if (err && err.code === "ECONNREFUSED") {
         if (ttl < 1) {
           return reject(new Error("DB didn\'t come up"));
         }
 
         console.log("Waiting for DB", ttl);
-        setTimeout(() => {_waitForDataBaseConnection(db, --ttl);}, 200);
+        setTimeout(() => {_waitForDataBaseConnection(db, --ttl);}, 10000);
         return;
       }
 
       if (err) return reject(err);
+
+      console.log('done?');
 
       resolve(null, true);
     });
