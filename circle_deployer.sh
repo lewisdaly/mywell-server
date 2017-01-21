b5c44fab60888cf7c8cc532131ad439c508e80f6
Lewis Daly <lewis.daly@enabled.com.au>
2017-01-21 12:13:04 +1030

    fix node stuff

--- circle_deployer.sh
+++ circle_deployer.sh
@@ -6,6 +6,10 @@ git pull origin master
 cd ~/mywell-server/mywell-ui
 git pull origin master
 
+#make sure that we have switched the right env variables
+cd ~/mywell-server/mywell-ui/ionic_rainapp
+npm run setup-prod
+
 echo 'deployment finished. Tailing docker logs to be sure'
 docker logs mywellserver_mywell-server_1 | tail -10
 docker logs mywellserver_mywell-ui_1 | tail -10
