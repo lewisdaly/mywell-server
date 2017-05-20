3b006954f9c75a835cd03aab0c3a45f802c6d775
Lewis Daly <lewis.daly@enabled.com.au>
2017-05-20 14:53:14 +0930

    Many ui changes, removing tail from circle deployer

--- circle_deployer.sh
+++ circle_deployer.sh
@@ -15,17 +15,5 @@ npm install
 npm run setup-prod
 npm run babel
 
-
 cd ~/mywell-server/
 ./run_server.sh production
-#make sure that we have switched the right env variables
-# cd ~/mywell-server/mywell-ui/src
-# npm run setup-prod
-
-# in /mywell-ui/src/:
-
-
-echo 'deployment finished. Tailing docker logs to be sure'
-docker logs mywellserver_mywell-server_1 | tail -10
-docker logs mywellserver_mywell-ui_1 | tail -10
-docker logs mywellserver_db_1 | tail -10
