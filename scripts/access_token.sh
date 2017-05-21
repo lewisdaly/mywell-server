a91134b39031f4dd65873b5f27ceb8a50d224312
Lewis Daly <lewis.daly@enabled.com.au>
2017-05-22 09:19:13 +0930

    Update access token script

--- scripts/access_token.sh
+++ scripts/access_token.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
-curl -# -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -H "Postman-Token: 39606c3a-fa33-b5db-67af-cd5fb6175acc" -d '  {"username":"marvi", "password":"marvi"}' "http://docker.local:3000/api/Users/login" | awk -F\" '{print $4}' | tail -1 | pbcopy
+
+if [ "$1" == local ]; then
+  HOST="http://docker.local:3000"
+else
+  HOST="https://mywell-server.marvi.org.in"
+fi
+
+curl -# -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -H "Postman-Token: 39606c3a-fa33-b5db-67af-cd5fb6175acc" -d '  {"username":"marvi", "password":"marvi"}' "$HOST/api/Users/login" | awk -F\" '{print $4}' | tail -1 | pbcopy
