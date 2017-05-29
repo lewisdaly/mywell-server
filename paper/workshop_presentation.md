aeb2ca5fab76c6067834283d39ce93a9e4c8d64a
Lewis Daly <lewis.daly@enabled.com.au>
2017-05-29 07:04:19 +0530

    Working on presentation

---
+++ paper/workshop_presentation.md
@@ -0,0 +1,41 @@
+
+__Before lunch: download the app__
+Link: https://docs.google.com/forms/d/e/1FAIpQLSfX4VBUaBTV8P4U4RQ6lpcKziorGKLTNkQvJf-JUn-L-DnH6g/viewform?usp=sf_link
+
+
+## Description
+- What is MyWell
+  - Technology component of MARVI
+  - Crowdsources well information from many people
+    - Give people ownership (no longer just scientists coming in - the villagers are the scientists)
+  - Smartphone and SMS app for tracking well readings, among other things
+    - Currrently, readings are being taken by hand for 10 villages. This obviously doesn't scale!
+      - more robust system
+      - scaleable system
+      - better visualisations
+    - IT to the rescue (and enter Lewis)
+
+  - How it works!
+    - BJ's travel around to wells, taking readings (this is currently being done on paper)
+    - Upload reading using SMS or app: (show example)
+    - Populates the map, and graphs etc.
+    - Also have the ability to track Rainfall levels and Checkdams (little dams)
+    - BJ's can also add new 'resources', by inputting a few details, such as LatLng,
+    - Attach pictures for a well
+    - View graphs and statistics
+    - Also support excel uploading for desktop...
+    - Information Flows:
+
+## Walkthrough
+
+
+## Demo
+- browsing
+- updating
+- settings
+-
+
+## Feedback
+- Display link
+https://goo.gl/zjxkqu
+-
--- scripts/upload_test_data.sh
+++ scripts/upload_test_data.sh
@@ -14,3 +14,76 @@ curl -X POST --header "Content-Type: application/json" --header "Accept: applica
   }
 }
 " "https://mywell-server.marvi.org.in/api/villages?access_token=O03DlocqsR2am6xwcWUrfhxSXsj1pZ6Jx59UIies8Tqhl6lIxwWLxho88WEAL5m1&access_token=O03DlocqsR2am6xwcWUrfhxSXsj1pZ6Jx59UIies8Tqhl6lIxwWLxho88WEAL5m1"
+
+
+{
+  "id": 1112,
+  "geo": {
+    "lat": 24.62392153690,
+    "lng": 74.131691982
+  },
+  "last_value": 0,
+  "well_depth": 35.34,
+  "last_date": "2016-05-11 03:04:59",
+  "owner": "Basant Ji",
+  "elevation": 0,
+  "type": "well",
+  "postcode": 333333,
+  "villageId": 11,
+  "villageId,postcode": 0
+}
+
+{
+  "id": 1113,
+  "geo": {
+    "lat": 24.61392153690,
+    "lng": 74.139991982
+  },
+  "last_value": 0,
+  "well_depth": 0,
+  "last_date": "2016-05-11 03:04:59",
+  "owner": "Basant Ji",
+  "elevation": 0,
+  "type": "checkdam",
+  "postcode": 333333,
+  "villageId": 11,
+  "villageId,postcode": 0
+}
+
+{
+  "id": 1113,
+  "geo": {
+    "lat": 24.61492153690,
+    "lng": 74.11231982
+  },
+  "last_value": 0,
+  "well_depth": 0,
+  "last_date": "2016-05-11 03:04:59",
+  "owner": "Basant Ji",
+  "elevation": 0,
+  "type": "raingauge",
+  "postcode": 333333,
+  "villageId": 11,
+  "villageId,postcode": 0
+}
+
+
+
+
+
+curl -X POST --header "Content-Type: application/json" --header "Accept: application/json" -d "{
+  \"id\": 1112,
+  \"geo\": {
+    \"lat\": 24.62392153690,
+    \"lng\": 74.131691982
+  },
+  \"last_value\": 0,
+  \"well_depth\": 35.34,
+  \"last_date\": \"2016-05-11 03:04:59\",
+  \"owner\": \"Basant Ji\",
+  \"elevation\": 0,
+  \"type\": \"well\",
+  \"postcode\": 333333,
+  \"villageId\": 11,
+  \"villageId,postcode\": 0
+}" "https://mywell-server.marvi.org.in/api/resources?access_token=O03DlocqsR2am6xwcWUrfhxSXsj1pZ6Jx59UIies8Tqhl6lIxwWLxho88WEAL5m1&access_token=O03DlocqsR2am6xwcWUrfhxSXsj1pZ6Jx59UIies8Tqhl6lIxwWLxho88WEAL5m1"
