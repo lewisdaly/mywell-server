1a245af93ff6255edd5a3a1e4a25221ce2883cd3
Lewis Daly <lewis.daly@enabled.com.au>
2017-06-27 22:02:39 +0930

    Try to fix credentials

--- deployment/tools/travis_encrypt.sh
+++ deployment/tools/travis_encrypt.sh
@@ -1,4 +1,6 @@
 #Run me from project root please! Not inside docker!
 
-tar cvf secrets.tar ~/.aws/keys/mywell-docker.pem ./env/.env
+cp ~/.aws/keys/mywell-docker.pem mywell-docker.pem
+tar cvf secrets.tar mywell-docker.pem ./env/.env
 travis encrypt-file secrets.tar --add
+rm -f mywell-docker.pem
