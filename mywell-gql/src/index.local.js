ae02631c6f42b6c64c7badd3b95ca6b52253a104
Lewis Daly <lewis.daly@enabled.com.au>
2017-07-26 17:35:47 +0930

    Got basic old node working

--- mywell-gql/src/index.local.js
+++ mywell-gql/src/index.local.js
@@ -5,23 +5,20 @@ const apolloServerExpress = require('apollo-server-express')
 const graphqlExpress = apolloServerExpress.graphqlExpress;
 const graphiqlExpress = apolloServerExpress.graphiqlExpress;
 
-const schema = require('./src/schema');
+const schema = require('./schema');
 
 const PORT = process.env.PORT;
 const app = express();
 
-schema.setUp()
-.then(_schema => {
-  app.get('/', function(req, res) {
-    res.send("Hello graphql");
-  });
+app.get('/', function(req, res) {
+  res.send("Hello graphql");
+});
 
-  app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: _schema }));
-  app.use('/graphiql', graphiqlExpress({
-    endpointURL: '/graphql',
-  }));
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: schema }));
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+}));
 
-  app.listen(PORT, function() {
-    console.log("server listening on port " + PORT);
-  });
+app.listen(PORT, function() {
+  console.log("server listening on port " + PORT);
 });
