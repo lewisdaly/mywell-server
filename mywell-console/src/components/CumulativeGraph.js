93a1746a0ce7f5b02d4d5c030cda4f9345ada590
Lewis Daly <lewis.daly@enabled.com.au>
2017-08-08 09:31:35 +0930

    making graph configurable

--- mywell-console/src/components/CumulativeGraph.js
+++ mywell-console/src/components/CumulativeGraph.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
-import { gql, graphql } from 'react-apollo'
+import { compose, gql, graphql } from 'react-apollo'
 import PropTypes from 'prop-types';
-import { VictoryAxis, VictoryLine, VictoryChart, VictoryGroup, VictoryBar } from 'victory';
+import { VictoryAxis, VictoryLine, VictoryTheme, VictoryChart, VictoryGroup, VictoryBar } from 'victory';
 import moment from 'moment';
 
 import { Loading } from './common'
@@ -9,8 +9,17 @@ import CumulativeGraphLine from './CumulativeGraphLine'
 
 class CumulativeGraph extends Component {
 
-  getTestChart() {
+  isLoading() {
+    if (this.props.thisYearQuery.loading ||
+        this.props.lastYearQuery.loading ||
+        this.props.twoYearQuery.loading) {
+          return true;
+        }
 
+    return false
+  }
+
+  getTestChart() {
     return (
       <VictoryChart>
         <VictoryGroup offset={20}
@@ -27,111 +36,116 @@ class CumulativeGraph extends Component {
     );
   }
 
-  render() {
-    if (this.props.data.loading || this.props.lastYearQuery.loading) {
-      return <Loading/>
-    }
+  transformGraphData(readings) {
+    return readings.map(reading => {
+      const date = moment(reading.week).format('D-M');
+      return {
+        x: date,
+        y: reading.value
+      };
+    });
+  }
 
-    const xTicks = this.props.data.cumulativeWeeklyReadings.map((reading, idx) => {
+  getXTicks() {
+    return this.props.thisYearQuery.cumulativeWeeklyReadings.map((reading, idx) => {
       if (idx % 10) {
         return null;
       }
       return moment(reading.week).format('MMM');
     });
+  }
 
-    const data = this.props.data.cumulativeWeeklyReadings.map(reading => {
-      const date = moment(reading.week).format('D-M-Y');
-      console.log(reading.value)
-      return {
-        x:reading.week,
-        y: reading.value
-        // value: idx * this.props.max
-      };
-    });
+  render() {
+    console.log("rendering!");
 
-    const data2 = this.props.lastYearQuery.cumulativeWeeklyReadings.map(reading => {
-      const date = moment(reading.week).format('D-M-Y');
-      return {
-        x:reading.week,
-        y: reading.value
-        // value: idx * this.props.max
-      };
-    })
+    if (this.isLoading()) {
+      return <Loading/>
+    }
 
-    console.log("xTicks:", xTicks);
-
-    //Hmm this is tricky - we need to plot all graphs on the same scale. This means finding the Max for 3 graphs (min will be 0),
-    //And scaling each one.
-    //Getting less and less confident that this will work.
+    const xTicks = this.getXTicks();
+    console.log(xTicks.length);
 
     return (
       <div>
-        {this.getTestChart()}
         <VictoryChart
-          domainPadding={20}
+          domainPadding={{y: 5}}
+          theme={VictoryTheme.material}
         >
-          {/* <VictoryAxis
-            // tickValues={xTicks}
-            // tickFormat={xTicks}
-          /> */}
+          <VictoryAxis
+            tickValues={xTicks}
+            tickFormat={xTicks}
+          />
           <VictoryAxis
             dependentAxis
-            // tickValues={}
+            style={{
+              axis: {stroke: "#756f6a"},
+              axisLabel: {fontSize: 15, padding: 30},
+              tickLabels: {fontSize: 10, padding: 5}
+            }}
             tickFormat={(y) => (y)}
-            domain={[0, 500  ]}
             label={"Cumulative Rainfall (mm)"}
           />
           <VictoryGroup
-            // colorScale={["tomato", "orange", "gold"]}
-            offset={20}
-            colorScale={"qualitative"}
             style={{
               parent: {
                 border: "1px solid #ccc"
               },
               data: {
-                fillOpacity: 0.0, stroke: "black", strokeWidth: 3
+                fillOpacity: 0.0, strokeWidth: 5
               }
             }}
           >
             <VictoryLine
-              style={this.props.style}
-              data={data}
+              data={this.transformGraphData(this.props.twoYearQuery.cumulativeWeeklyReadings)}
+              labels={datum => {
+                if (datum.eventKey === Math.floor((xTicks.length - 10) / 3)) {
+                  return '2 Years Ago';
+                }
+                return '';
+              }}
+              style={{
+                parent: {
+                  border: "1px solid #ccc"
+                },
+                labels: {
+                  fontSize: 10,
+                }
+              }}
             />
             <VictoryLine
-              style={this.props.style}
-              data={data2}
-            />
-            {/* <CumulativeGraphLine
-              // style={{
-              //   data: { stroke: "#c43a31" },
-              // }}
-              resourceId={this.props.resourceId}
-              postcode={this.props.postcode}
-              startDate={moment('2016-08-01').toDate()}
-              endDate={moment('2017-08-01').toDate()}
-              max={10}
-            /> */}
-            {/* <CumulativeGraphLine
-              style={{
-                data: { stroke: "#a12b31" },
-                fillOpacity: 0.5
+              data={this.transformGraphData(this.props.lastYearQuery.cumulativeWeeklyReadings)}
+              labels={datum => {
+                if (datum.eventKey === Math.floor((xTicks.length - 2) / 3) * 2 ) {
+                  return 'Last Year';
+                }
+                return '';
               }}
-              resourceId={this.props.resourceId}
-              postcode={this.props.postcode}
-              startDate={moment('2015-08-01').toDate()}
-              endDate={moment('2016-08-01').toDate()}
-              max={100}
+              style={{
+                parent: {
+                  border: "1px solid #ccc"
+                },
+                labels: {
+                  fontSize: 10,
+                }
+              }}
             />
-            <CumulativeGraphLine
-              // style={{
-              //   data: { stroke: "#f43a31" }
-              // }}
-              resourceId={this.props.resourceId}
-              postcode={this.props.postcode}
-              startDate={moment('2014-08-01').toDate()}
-              endDate={moment('2015-08-01').toDate()}
-            /> */}
+            <VictoryLine
+              data={this.transformGraphData(this.props.thisYearQuery.cumulativeWeeklyReadings)}
+              labels={datum => {
+                if (datum.eventKey === Math.floor(xTicks.length - 2)) {
+                  return 'This Year';
+                }
+                return '';
+              }}
+              style={{
+                parent: {
+                  border: "1px solid #ccc"
+                },
+                labels: {
+                  fontSize: 10,
+                }
+              }}
+            />
           </VictoryGroup>
         </VictoryChart>
       </div>
@@ -142,30 +156,19 @@ class CumulativeGraph extends Component {
 CumulativeGraph.propTypes = {
   resourceId: PropTypes.number,
   postcode: PropTypes.number,
+  startDates: PropTypes.arrayOf(PropTypes.instanceOf(Date)),
+  endDates: PropTypes.arrayOf(PropTypes.instanceOf(Date)),
 }
 
-const CumulativeQuery = gql`
-  query cumulativeWeeklyReadings($resourceId: Int!, $postcode: Int!, $startDate: DateTime!, $endDate: DateTime!) {
+const ThisYearQuery = gql`
+  query thisYearCumulativeWeeklyReadings($resourceId: Int!, $postcode: Int!, $startDate: DateTime!, $endDate: DateTime!) {
     cumulativeWeeklyReadings(resourceId:$resourceId, postcode:$postcode, startDate:$startDate, endDate:$endDate) {
       week
       value
     }
   }
 `
-
-
-const GraphPageWithCumulativeQuery = graphql(CumulativeQuery, {
-  options: () => ({
-    variables: {
-      resourceId:1111,
-      postcode:313603,
-      startDate: moment('2014-08-01').toDate(),
-      endDate: moment('2015-08-01').toDate()
-    }
-  })
-})(CumulativeGraph);
-
-const LastYearCumulativeQuery = gql`
+const LastYearQuery = gql`
   query lastYearCumulativeWeeklyReadings($resourceId: Int!, $postcode: Int!, $startDate: DateTime!, $endDate: DateTime!) {
     cumulativeWeeklyReadings(resourceId:$resourceId, postcode:$postcode, startDate:$startDate, endDate:$endDate) {
       week
@@ -173,16 +176,50 @@ const LastYearCumulativeQuery = gql`
     }
   }
 `
-const GraphPageWithLastYearCumulativeQuery = graphql(LastYearCumulativeQuery, {
-  name: 'lastYearQuery',
-  options: () => ({
-    variables: {
-      resourceId:1111,
-      postcode:313603,
-      startDate: moment('2015-08-01').toDate(),
-      endDate: moment('2016-08-01').toDate()
-    }
-  })
-})(GraphPageWithCumulativeQuery);
 
-export default GraphPageWithLastYearCumulativeQuery;
+const TwoYearQuery = gql`
+  query twoYearCumulativeWeeklyReadings($resourceId: Int!, $postcode: Int!, $startDate: DateTime!, $endDate: DateTime!) {
+    cumulativeWeeklyReadings(resourceId:$resourceId, postcode:$postcode, startDate:$startDate, endDate:$endDate) {
+      week
+      value
+    }
+  }
+`
+
+const CumulativeGraphWithQueries = compose(
+  graphql(ThisYearQuery, {
+    name: 'thisYearQuery',
+    options: ({ postcode, resourceId, startDates, endDates }) => ({
+      variables: {
+        postcode,
+        resourceId,
+        startDate: startDates[0],
+        endDate: endDates[0]
+      }
+    })
+  }),
+  graphql(LastYearQuery, {
+    name: 'lastYearQuery',
+    options: ({ postcode, resourceId, startDates, endDates }) => ({
+      variables: {
+        postcode,
+        resourceId,
+        startDate: startDates[1],
+        endDate: endDates[1]
+      }
+    })
+  }),
+  graphql(TwoYearQuery, {
+    name: 'twoYearQuery',
+    options: ({ postcode, resourceId, startDates, endDates }) => ({
+      variables: {
+        postcode,
+        resourceId,
+        startDate: startDates[2],
+        endDate: endDates[2]
+      }
+    })
+  })
+)(CumulativeGraph);
+
+export default CumulativeGraphWithQueries;
--- mywell-console/src/components/common/index.js
+++ mywell-console/src/components/common/index.js
@@ -2,8 +2,10 @@ import Loading from './Loading'
 import Nav from './Nav'
 import ThreadImage from './ThreadImage'
 import UserCard from './UserCard'
+import SelectButton from './SelectButton'
 
 module.exports = {
+  SelectButton: SelectButton,
   Loading: Loading,
   Nav: Nav,
   ThreadImage: ThreadImage,
