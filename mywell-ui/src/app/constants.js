d3a58fe57720860228505a759979323ee5ce824f
Lewis Daly <lewis.daly@enabled.com.au>
2017-08-11 22:01:57 +0930

    add new mywell-ui

---
+++ mywell-ui/src/app/constants.js
@@ -0,0 +1,2 @@
+angular.module('module.constants',[])
+  .constant('debug', '1')
---
+++ mywell-ui/src/app/service/UserService.js
@@ -0,0 +1,47 @@
+angular.module('service.user', [])
+.service('UserService', function($http, $q, $rootScope) {
+
+	return({
+		getUser: getUser,
+		getUserDetails: getUserDetails,
+		getCurrentUserID: getCurrentUserID
+	});
+
+
+	//Gets any user
+	function getUser(userID) {
+		//If we haven't specified a user, return the current user
+		if (!userID || userID === -1){userID = getCurrentUserID();}
+
+
+		var baseUrl = SERVER_URL;
+
+		return $http({
+			method: "get",
+			headers: {'Content-Type': 'application/json'},
+	      	url: baseUrl + "/api/user/" + userID
+		})
+		.success(function(data) {
+
+		})
+		.error(function() {
+
+		});
+	}
+
+	//Gets the details for the current logged in user
+	function getUserDetails() {
+		var baseUrl = SERVER_URL;
+
+		return $http({
+			method: "get",
+			url: baseUrl + "/api/user/current"
+		})
+	}
+
+	//Private Methods
+	function getCurrentUserID() {
+		var currentUser = $rootScope.globals.currentUser;
+		return currentUser.userID;
+	}
+});
