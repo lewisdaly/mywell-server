7ad13c68ad35b6c3495b6f6d657356fbefa9d6b7
Lewis Daly <lewis.daly@enabled.com.au>
2017-08-11 20:35:11 +0930

    fix production build temporarily

--- mywell-ui-2/src/app/controller/SignupController.js
+++ mywell-ui-2/src/app/controller/SignupController.js
@@ -1,19 +1,11 @@
 angular.module('controller.signup',[])
-.controller('SignupController', function($scope, $location, SignupService, AuthenticationService, $window, debug) {
+.controller('SignupController', function($scope, $location, SignupService, AuthenticationService, $window) {
 	$scope.user = {};
 
 	$scope.signup = function() {
     //TODO: Verify the email, passwords etc.
     //TODO: fix: for some reason email is coming back null..
     $window.location.reload(true);
-
-    //Always log in in debug mode!
-     if (debug == 1) {
-        AuthenticationService.SetCredentials("lewis", 1, "password");
-        $location.path('/tab/map');
-        return;
-    }
-
 
     SignupService.signUp($scope.user)
     .then(function(data) {
--- mywell-ui-2/webpack.config.js
+++ mywell-ui-2/webpack.config.js
@@ -18,8 +18,9 @@ const enableSourceMaps = true;
 
 var ENV = process.env.npm_lifecycle_event;
 
-var isTest = ENV === 'test' || ENV === 'test-watch';
-var isProd = ENV === 'build';
+//Hardcoded for now - I don't like implicit settings like this.
+var isTest = false;
+var isProd = false;
 
 module.exports = function makeWebpackConfig() {
   var config = {};
