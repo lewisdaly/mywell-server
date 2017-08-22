571fdd8fa14b63e7d9afafc6aa1252bef9ab9b32
Lewis Daly <lewis.daly@enabled.com.au>
2017-08-22 21:29:23 +0930

    Fix travis config

--- mywell-ui/post_build_deploy.sh
+++ mywell-ui/post_build_deploy.sh
@@ -15,15 +15,24 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #build - use the overide file to ensure we build webpack
 # rm -rf /tmp/www && mkdir /tmp/www
-docker-compose -f "$DIR"/../docker-compose.yml -f "$DIR"/../docker-compose.ui.yml up mywell-ui
+# docker-compose -f "$DIR"/../docker-compose.ui.yml up mywell-ui || exit 1
 
-#run again to get the www folder
+#we can't use docker-compose as we don't want to mount the volumes
 docker rm -f "$BUILD_IMAGE_NAME"
-docker run -d --name "$BUILD_IMAGE_NAME" mywell-ui:local bash -c "tail -f /dev/null"
+docker run --rm \
+  -e SERVER_URL \
+  -e VERSION_NUMBER \
+  -e REACT_APP_GRAPHQL_ENDPOINT \
+  --name "$BUILD_IMAGE_NAME" mywell-ui:local
+
+docker rm -f "$BUILD_IMAGE_NAME"
+docker run --rm -d --name "$BUILD_IMAGE_NAME" mywell-ui:local bash -c "tail -f /dev/null"
 docker cp "$BUILD_IMAGE_NAME":/usr/src/app/www /tmp/
 
 #this gets copied to local as we mount the dir
 # cp -R "$DIR"/www /tmp/www
+
+#if this fails, its ok - we can do it later on
 aws s3 sync /tmp/www s3://"$UI_BUCKET_NAME"
 
 exit 0
