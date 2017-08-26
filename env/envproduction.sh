91b5b8e77ccb9599914a95320b615b6b41faf797
Lewis Daly <lewis.daly@enabled.com.au>
2017-08-26 16:17:00 +0930

    Fix env variables for production

--- env/envproduction.sh
+++ env/envproduction.sh
@@ -16,10 +16,11 @@ export IMAGE_SUFFIX="production"
 export VERSION_NUMBER="1.3.6"
 export SERVER_PORT=3000
 export UI_PORT=8000
+export GQL_PORT=3003
 export DONE_SEEDING_SUFFIX="production_done"
 export TEMP_SERVER_FRONTEND_RULE="Host:${STAGE_PREFIX}mywell-server.vessels.tech,dev-mywell-server.marvi.org.in,mywell-server.marvi.org.in"
 export REACT_APP_GRAPHQL_ENDPOINT="https://mywell-gql.vessels.tech/graphql"
-export UI_BUCKET_NAME="test-mywell.vessels.tech"
+export UI_BUCKET_NAME="mywell.vessels.tech"
 
 
 source $DIR/env.sh
