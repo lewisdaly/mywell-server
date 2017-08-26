a3296d93c8acac76a7546ae7f435b2b83faa7b73
Lewis Daly <lewis.daly@enabled.com.au>
2017-08-26 15:00:44 +0930

    work on dns config

--- env/envdevelopment.sh
+++ env/envdevelopment.sh
@@ -6,7 +6,7 @@ export ENABLE_NOTIFICATIONS=false
 export ENABLE_LOGS=true
 export ENVIRONMENT=development
 export REFRESH_UI=false
-export SERVER_URL=https://dev2-mywell-server.marvi.org.in
+export SERVER_URL=https://dev2-mywell-server.vessels.tech
 export STAGE_PREFIX="dev2-"
 export DB_HOST="mywelldb.cyftlfi9bxci.ap-southeast-2.rds.amazonaws.com"
 export DB_NAME="development_mywell"
@@ -17,9 +17,9 @@ export VERSION_NUMBER="dev_1.3.6"
 export SERVER_PORT=3002
 export UI_PORT=8002
 export DONE_SEEDING_SUFFIX="development_done"
-export TEMP_SERVER_FRONTEND_RULE="Host:${STAGE_PREFIX}mywell-server.marvi.org.in"
-export REACT_APP_GRAPHQL_ENDPOINT="https://dev2-mywell-gql.marvi.org.in/graphql"
-export UI_BUCKET_NAME="dev2-mywell.marvi.org.in"
+export TEMP_SERVER_FRONTEND_RULE="Host:${STAGE_PREFIX}mywell-server.vessels.tech,${STAGE_PREFIX}mywell-server.marvi.org.in"
+export REACT_APP_GRAPHQL_ENDPOINT="https://dev2-mywell-gql.vessels.tech/graphql"
+export UI_BUCKET_NAME="dev2-mywell.vessels.tech"
 
 
 
--- env/envproduction.sh
+++ env/envproduction.sh
@@ -6,7 +6,7 @@ export ENABLE_NOTIFICATIONS=true
 export ENABLE_LOGS=false
 export ENVIRONMENT=production
 export REFRESH_UI=false
-export SERVER_URL=https://mywell-server.marvi.org.in
+export SERVER_URL=https://mywell-server.vessels.tech
 export STAGE_PREFIX=""
 export DB_HOST="mywelldb.cyftlfi9bxci.ap-southeast-2.rds.amazonaws.com"
 export DB_NAME="production_mywell"
@@ -17,9 +17,9 @@ export VERSION_NUMBER="1.3.6"
 export SERVER_PORT=3000
 export UI_PORT=8000
 export DONE_SEEDING_SUFFIX="production_done"
-export TEMP_SERVER_FRONTEND_RULE="Host:${STAGE_PREFIX}mywell-server.marvi.org.in,dev-mywell-server.marvi.org.in"
-export REACT_APP_GRAPHQL_ENDPOINT="https://mywell-gql.marvi.org.in/graphql"
-export UI_BUCKET_NAME="test-mywell.marvi.org.in"
+export TEMP_SERVER_FRONTEND_RULE="Host:${STAGE_PREFIX}mywell-server.vessels.tech,dev-mywell-server.marvi.org.in,mywell-server.marvi.org.in"
+export REACT_APP_GRAPHQL_ENDPOINT="https://mywell-gql.vessels.tech/graphql"
+export UI_BUCKET_NAME="test-mywell.vessels.tech"
 
 
 source $DIR/env.sh
