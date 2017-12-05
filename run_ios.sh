ba47818b18619ed7eebcba4cf23b4c9d235fd875
Lewis Daly <lewis.daly@enabled.com.au>
2017-12-05 20:45:36 +0800

    work on run ios script

--- run_ios.sh
+++ run_ios.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MYWELL_UI_DIR="$DIR"/mywell-ui/src
+
 export VERSION_NUMBER=1.3.1
 export ENABLE_LOGS=true
 
-cd /Users/lewis/developer/mywell/src/mywell-ui/src
+cd "$MYWELL_UI_DIR"
 gulp replace --env prod
 gulp babel || exit 1
 
