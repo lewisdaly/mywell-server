b590300f425a1df2edaecee8a60dd9a057ff473f
Lewis Daly <lewis.daly@enabled.com.au>
2017-12-05 21:07:00 +0800

    work on run ios script

--- run_ios.sh
+++ run_ios.sh
@@ -3,11 +3,11 @@ export VERSION_NUMBER=1.3.1
 export ENABLE_LOGS=true
 
 cd /Users/lewis/developer/mywell/src/mywell-ui/src
-gulp replace --env prod
-gulp babel || exit 1
+# gulp replace --env prod
+# gulp babel || exit 1
 
 if [ "$1" == "emulator" ]; then
-  ionic run ios --emulator
+  ionic cordova run ios --emulator
 fi
 
-ionic run ios --device
+ionic cordova run ios --device
