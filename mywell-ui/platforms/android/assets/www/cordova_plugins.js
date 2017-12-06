342531e0d912dc803ce0d54c0934bfc418d88759
Lewis Daly <lewis.daly@enabled.com.au>
2017-12-06 18:07:30 +0800

    hide statusbar

--- mywell-ui/platforms/android/assets/www/cordova_plugins.js
+++ mywell-ui/platforms/android/assets/www/cordova_plugins.js
@@ -13,7 +13,8 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.2",
-    "cordova-plugin-statusbar": "2.3.1-dev"
+    "cordova-plugin-statusbar": "2.3.1-dev",
+    "cordova-plugin-hidden-statusbar": "1.0.0"
 };
 // BOTTOM OF METADATA
 });
